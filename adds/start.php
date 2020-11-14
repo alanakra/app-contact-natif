73eb2de33e4f4d162caf310ec5028f134c9378dd
Alan <alanakra@hotmail.fr>
2020-11-14 17:35:03 +0100

    Update start.php

--- adds/start.php
+++ adds/start.php
@@ -1,7 +1,5 @@
 <?php 
-$port = '8889';
-$password = 'root';
 
-$db = new PDO('mysql:host=localhost;dbname=app-contact-db;port=3306; charset=UTF8', 'root', '');
+$db = new PDO('mysql:host=localhost;dbname=app-contact-db;port=8383; charset=UTF8', 'root', 'password');
 
 ?>
